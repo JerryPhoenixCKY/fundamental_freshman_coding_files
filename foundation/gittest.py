fb6482123d28fa4640ac609e2641249772663d47
CKY <luckynatic@gmail.com>
2026-05-17 11:18:51 +0800

    test file for git and github

---
+++ foundation/gittest.py
@@ -0,0 +1,5 @@
+#it is a file to test git and help me to learn how to use git and github
+print("Hello, this is a test file for git and github.")
+print("I am learning how to use git and github to manage my code and collaborate with others.")
+print("I will create a repository on github and push this file to it.")
+print("I will also create a branch and make some changes to this file, and then merge it back to the main branch.")
